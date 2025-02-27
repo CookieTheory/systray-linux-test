5f9a78f04ba2b00e156b0a69f66303e39dfd9349
CookieTheory <118179323+CookieTheory@users.noreply.github.com>
2025-02-27 21:49:51 +0100

    Initial commit

---
+++ src/main/java/hr/linux/systray_linux/SystrayLinuxApplication.java
@@ -0,0 +1,13 @@
+package hr.linux.systray_linux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystrayLinuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystrayLinuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/linux/systray_linux/SystrayLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.linux.systray_linux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystrayLinuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
